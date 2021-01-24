3a5adf8878a724d567b8df9b416291bc08b6ed61
subhampreet <subhampreet18@gmail.com>
2021-01-24 19:24:32 +0530

    Routing Added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
+import NotFound from "./NotFound";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
 
 const App = () => (
   <BrowserRouter>
     <Header />
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route  component={NotFound} />
+      </Switch>
+    </main>
   </BrowserRouter>
 );
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,24 +7,29 @@ function Header() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid ">
-                    <Link className="navbar-brand font-weight-bold" to="#">YouTees</Link>
+                    <Link className="navbar-brand font-weight-bold" to="/">YouTees</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                            <Link className="nav-link" aria-current="page" to="#">Sign Up</Link>
-                            </li>
-                            <li className="nav-item">
-                            <Link className="nav-link" to="#">Sign In</Link>
-                            </li>
-                           
-                        </ul>
-                        <form className="d-flex">
+                        
+                        <form className="m-auto d-flex mt-1">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
+
+                        <ul className="navbar-nav d-flex mb-2 mb-lg-0">
+                            <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/signup">Sign Up</Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link className="nav-link" to="/signin">Sign In</Link>
+                            </li>
+                           
+                        </ul>
                     </div>
                 </div>
             </nav> 
